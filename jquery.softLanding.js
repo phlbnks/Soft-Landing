c66071955442e168f4101ad339ba6b9a22f15b72
emirpprime <stickmanphotography@gmail.com>
2011-03-25 15:31:52 +0000

    First draft

--- jquery.softLanding.js
+++ jquery.softLanding.js
@@ -1,3 +1,5 @@
+//softLanding v0.1
+
 $(document).ready(function(){
 	// Take an item, if the viewport scrolls past it's position, pin it to the top of the viewport
 	//** This version of the code by jakub@linowski.ca 
@@ -10,7 +12,7 @@ $(document).ready(function(){
 			$('#nav').css({'position':'absolute','top':'320px'});
 		}
 	});
-	// Here we animate the move to anchor, and stop a sudden stop.
+	// Here we animate the move to anchor, and prevent a sudden stop.
 	//** by Phil Banks http://customcreative.co.uk //
 	//Bind to #links
 	$('a[href^="#"]').click(function(){
@@ -20,12 +22,12 @@ $(document).ready(function(){
 		var vHeight = $(window).height(); // height of browser viewport
 		//If the destination coordinates are not in the last viewport's worth of document height then scroll to them.
 		if (dest < (dHeight-vHeight)) {
-			$('html, body').stop().animate({scrollTop: dest},{duration:1500,easing: 'easeOutCirc'});
+			$('html, body').stop().animate({scrollTop: dest},1500,'easeOutCirc');
 		}
 			//Otherwise define 'dest' as the top of the last viewport's worth of document height and scroll there. This prevents the animation trying to scroll past the end of the document and causing a sudden stop.
 			else {
 				var dest = (dHeight-vHeight);
-				$('html, body').stop().animate({scrollTop: dest},{duration:1500,easing: 'easeOutCirc'});
+				$('html, body').stop().animate({scrollTop: dest},1500,'easeOutCirc');
 			}
 		return false;
 	});
